c070e11587a4329c0c5f98ac607e6143af03a47c
Awais2512 <218716146+Awais-Mindrind@users.noreply.github.com>
2025-09-12 12:22:38 +0500

    empty coutry field handled

--- src/airtable.py
+++ src/airtable.py
@@ -68,7 +68,7 @@ def get_active_hashtags():
             fields = record["fields"]
             hashtag = fields.get("Hashtag")
             is_active = fields.get("Active", False)
-            countries_str = fields.get("Countries", ["usa", "uk", "canada", "australia", "germany", "france", "italy", "spain", "japan", "china", "india", "brazil", "mexico", "russia", "southkorea", "uae", "saudiarabia", "turkey", "indonesia", "singapore"])
+            countries_str = fields.get("Countries", "usa, uk, canada, australia, germany, france, italy, spain, japan, china, india, brazil, mexico, russia, southkorea, uae, saudiarabia, turkey, indonesia, singapore")
             minimum_followers = fields.get("Minimum_Followers", 5000)
             
             # Only include hashtags that are active (True/checked)
